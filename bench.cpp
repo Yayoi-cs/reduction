e9508d52ea8f0eafadea702f6f0a7b7757fea305
Yayoi-cs <hirogoshawk3249@gmail.com>
2026-03-27 16:28:51 +0900

    modify bench.cpp

--- bench.cpp
+++ bench.cpp
@@ -7,37 +7,40 @@
 #include <numeric>
 #include <execution>
 
-#define N_OBJ 0x1000
 #define N_TIMES 0x1000000
 
 int main(void) {
-    auto rand_ptr = benchlib::rand_buffer<double>(N_OBJ);
-    double* p = rand_ptr.get();
-    double sum = 0.0;
+    for (size_t i = 0x40;i < 0x4000;i=i*2) {
+        auto rand_ptr = benchlib::rand_buffer<double>(i);
+        double* p = rand_ptr.get();
+        double sum = 0.0;
+        printf("i: %lx\n",i);
+        sum = 0.0;
+        auto r1 = benchlib::measure([&]() {
+            sum += reduction_f64(p, i);
+        }, N_TIMES);
+        printf("reduction_f64:  %ld ns  (sum: %A)\n", r1.count(), sum);
 
-    sum = 0.0;
-    auto r1 = benchlib::measure([&]() {
-        sum += reduction_align_f64(p, N_OBJ);
-    }, N_TIMES);
-    printf("reduction_f64:  %ld ns  (sum: %A)\n", r1.count(), sum);
+        sum = 0.0;
+        auto r2 = benchlib::measure([&]() {
+            sum += reduction_align_f64(p, i);
+        }, N_TIMES);
+        printf("reduction_align_f64:  %ld ns  (sum: %A)\n", r2.count(), sum);
 
-    sum = 0.0;
-    auto r2 = benchlib::measure([&]() {
-        sum += reduction_align_f64(p, N_OBJ);
-    }, N_TIMES);
-    printf("reduction_align_f64:  %ld ns  (sum: %A)\n", r2.count(), sum);
+        sum = 0.0;
+        auto r3 = benchlib::measure([&]() {
+            sum += reduction_align_64n_f64(p, i);
+        }, N_TIMES);
+        printf("reduction_align_64n_f64:  %ld ns  (sum: %A)\n", r3.count(), sum);
 
-    sum = 0.0;
-    auto r3 = benchlib::measure([&]() {
-        sum += reduction_align_64n_f64(p, N_OBJ);
-    }, N_TIMES);
-    printf("reduction_align_64n_f64:  %ld ns  (sum: %A)\n", r3.count(), sum);
+        sum = 0.0;
+        auto r4 = benchlib::measure([&]() {
+            sum += std::reduce(std::execution::unseq, p, p + i);
+        }, N_TIMES);
+        printf("std::reduce:  %ld ns  (sum: %A)\n", r4.count(), sum);
 
-    sum = 0.0;
-    auto r4 = benchlib::measure([&]() {
-        sum += std::reduce(std::execution::unseq, p, p + N_OBJ);
-    }, N_TIMES);
-    printf("std::reduce:  %ld ns  (sum: %A)\n", r4.count(), sum);
+        puts("");
+    }
 
     return 0;
 }
